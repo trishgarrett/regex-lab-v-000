b79b884c621b4dcee797229e25a002cec8ae086d
Trisha Garrett <trisha.garrett@gmail.com>
2017-11-17 21:30:27 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[aeiouAEIOU]\w+/)
+    true
+  else
+    false  
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -7,6 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
+
 
 end
 
