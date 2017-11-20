de87604a448a219eb10f186d148662e452ada8f4
Trisha Garrett <trisha.garrett@gmail.com>
2017-11-20 16:27:16 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -2,16 +2,16 @@ def starts_with_a_vowel?(word)
   if word.match(/^[aeiouAEIOU]\w+/)
     true
   else
-    false  
+    false
   end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+   text.scan (/[un]{2}\w+[ing]{3}/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan (/\b\w{5}\b/)
 
 end
 
